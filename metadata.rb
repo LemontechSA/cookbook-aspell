b790cf210625c92bb931b594b362132cf9eb1668
Sean Culver <sean@codingzeal.com>
2013-06-19 09:51:38 -0700

    Adding meta data

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Sean Culver"
+maintainer_email "seanculver@gmail.com"
+license          "GPL"
+description      "Installs/Configures aspell"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
